b2462b0325e8a9e593ad406a656da5a81cf6006c
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 <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>
2024-01-31 08:25:13 -0500

    $[Dr.Wise]@(Kali)//:{Holberton}$

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,24 +1,21 @@
 #!/usr/bin/node
-const reqUrl = process.argv[2];
-const req = require('request');
 
-req.get(reqUrl, (err, res, body) => {
-  if (err) {
-    console.error(err);
-  } else {
-    const movies = JSON.parse(body);
-    const WedgeAntilles = 'https://swapi-api.hbtn.io/api/people/18/';
-    const chars = movies.results[0].characters;
-    if (chars.includes(WedgeAntilles)) {
-      req.get(WedgeAntilles, (erro, resp, Body) => {
-        if (erro) {
-          console.error(erro);
-        } else {
-          const wedge = JSON.parse(Body);
-          const filmsCount = wedge.films.length;
-          console.log(filmsCount);
-        }
-      });
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  const movies = JSON.parse(body).results;
+  let count = 0;
+
+  for (const movie of movies) {
+    const characters = movie.characters;
+    if (characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
+      count++;
     }
   }
+  console.log(count);
 });
