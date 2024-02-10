4ab467ee82e0bcc3df1927f1ef1fdd6d1bcc5d29
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 <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>
2024-02-10 14:25:23 -0500

    $[Dr.Wise]@(Kali)//:{Holberton}$

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 // a script that writes a string to a file.
 const fs = require('fs');
+
 const fileName = process.argv[2];
 fs.readFile(fileName, 'utf8', (error, data) => {
   if (error) {
