750660122628913e8ddc5f86cd24c74442c7bea1
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 <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>
2024-03-21 16:29:16 -0400

    $[Dr.Wise]@(Kali)//:{Holberton}$

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,15 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.resume();
+
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
+
   if (name) {
     process.stdout.write(`Your name is: ${name}`);
-    process.stdout.write('This important software is now closing\n');
-    process.exit();
   }
 });
+
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
